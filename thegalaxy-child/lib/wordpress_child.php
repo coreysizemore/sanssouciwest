80c0531971e8e0ca1b580c66e0306ce77242c546
Corey Sizemore <coreysizemore@gmail.com>
2021-01-27 12:10:42 -0800

    Updating Child Theme

--- thegalaxy-child/lib/wordpress_child.php
+++ thegalaxy-child/lib/wordpress_child.php
@@ -1,28 +1,18 @@
 <?php
 	
 	/*
-		Child Wordpress Funtions
+		Call Custom Styles
 	*/
 	
-	function csdd_child_scripts() {
-	
-		wp_enqueue_style( 'csdd-custom', get_stylesheet_directory_uri() . '/css/custom.css' );
-	
-	}
-	add_action( 'wp_enqueue_scripts', 'csdd_child_scripts' );
-	
-	if( function_exists('acf_add_options_page') ) {
-		
-		acf_add_options_page(array(
-			'page_title' => 'Available Homes',
-			'menu_title' => 'Available Homes',
-			'menu_slug' => 'available-homes',
-			'capability' => 'edit_posts',
-			'parent_slug' => '',
-			'position' => false,
-			'icon_url' => false,
-			'redirect' => false
-		));
-	}
 
+	function galaxy_scriptss() 
+	{
+		
+		wp_enqueue_style( 'galaxy-child-style', get_stylesheet_directory_uri() . '/style.css' );
+	    wp_enqueue_style( 'galaxy-child-custom-style', get_stylesheet_directory_uri() . '/css/custom.css' );
+	    
+	}
+	
+	add_action( 'wp_enqueue_scripts', 'galaxy_scriptss', 99 );
+	
 ?>
